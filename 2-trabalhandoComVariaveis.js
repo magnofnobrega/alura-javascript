18124d63a6d5f99b7649f77372e3e0d87fa66025
Magno F. Nóbrega <magnofnobrega@outlook.com>
2023-02-11 17:05:43 -0300

    Trabalhando com variáveis

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+console.log('Trabalhando com variáveis');
+
+//js é case sensitive
+
+const idade = 26;
+const nome = "Magno";
+let ano = 2023;
+
+console.log(idade);
+console.log(nome);
+console.log(ano);
